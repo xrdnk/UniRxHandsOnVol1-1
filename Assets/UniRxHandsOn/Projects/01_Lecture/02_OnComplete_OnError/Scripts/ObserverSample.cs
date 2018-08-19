ac14546eda742fdcedcd13f0e7665c9d6b5cf1ce
ShojiOshima <oc1shoji@gmail.com>
2018-08-19 22:57:48 +0900

    参照剥がれ修正と説明追加

---
+++ Assets/UniRxHandsOn/Projects/01_Lecture/02_OnComplete_OnError/Scripts/ObserverSample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace UniRxHandsOn.Lecture02
+{
+	public class ObserverSample : MonoBehaviour
+	{
+		[SerializeField] private SubjectSample m_SubjectSample = null;
+
+		private void Awake()
+		{
+			// Subjectの購読及び、振る舞い定義
+			m_SubjectSample.OnValueChanged
+				.Subscribe(
+					msg => Debug.Log( "OnNext:" + msg ),
+					error => Debug.Log( "OnError: " + error ),
+					() => Debug.Log( "OnCompleted" ) );
+		}
+	}
+}
---
+++ Assets/UniRxHandsOn/Projects/01_Lecture/02_OnComplete_OnError/Scripts/SubjectSample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace UniRxHandsOn.Lecture02
+{
+	public class SubjectSample : MonoBehaviour
+	{
+		// Subjectの定義
+		private Subject<int> m_Subject = new Subject<int>();
+		public IObservable<int> OnValueChanged { get { return m_Subject; } }
+
+		private void Start()
+		{
+			// イベントの発火処理
+			m_Subject.OnNext( 1 );
+			m_Subject.OnCompleted();
+			m_Subject.OnNext( 3 );
+		}
+	}
+}
