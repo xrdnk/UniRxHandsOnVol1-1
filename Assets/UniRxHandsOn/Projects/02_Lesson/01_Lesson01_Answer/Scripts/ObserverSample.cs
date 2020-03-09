855806252b04caae5397b3e588f1700b335135c7
xrdnk <xrdnk1010@gmail.com>
2020-03-09 23:58:49 +0900

    test

--- Assets/UniRxHandsOn/Projects/02_Lesson/01_Lesson01_Answer/Scripts/ObserverSample.cs
+++ Assets/UniRxHandsOn/Projects/02_Lesson/01_Lesson01_Answer/Scripts/ObserverSample.cs
@@ -3,24 +3,24 @@ using UniRx;
 
 namespace UniRxHandsOn.Lesson_01_Answer
 {
-	public class ObserverSample : MonoBehaviour
-	{
-		[SerializeField] private SubjectSample m_SubjectSample = null;
+    public class ObserverSample : MonoBehaviour
+    {
+        [SerializeField] private SubjectSample m_SubjectSample = null;
 
-		private void Awake()
-		{
-			// Subjectの購読及び、振る舞い定義
-			m_SubjectSample.m_Subject
-				.Subscribe( ( x ) => Debug.Log( x ) );
+        private void Awake()
+        {
+            // Subjectの購読及び、振る舞い定義
+            m_SubjectSample.m_Subject
+                .Subscribe((x) => Debug.Log(x));
 
 
-			// 重要: Subjectの実体を外部に公開してしまうと
-			//		 下記のようなコードが通ってしまうため危険。
-			//		 よりBetterな解答例は、本ディレクトリの「Better」を参照
-			// m_SubjectSample.m_Subject.OnNext( 1 );	// Observer側が勝手にメッセージを送信できてしまうため、
-														// どこからメッセージが発生しているのか分からなくなる
+            // 重要: Subjectの実体を外部に公開してしまうと
+            //		 下記のようなコードが通ってしまうため危険。
+            //		 よりBetterな解答例は、本ディレクトリの「Better」を参照
+            // m_SubjectSample.m_Subject.OnNext( 1 );	// Observer側が勝手にメッセージを送信できてしまうため、
+            // どこからメッセージが発生しているのか分からなくなる
 
-			// m_SubjectSample.m_Subject = null;		// Observer側が勝手にSubjectを抹消できてしまう
-		}
-	}
+            // m_SubjectSample.m_Subject = null;		// Observer側が勝手にSubjectを抹消できてしまう
+        }
+    }
 }
--- Assets/UniRxHandsOn/Projects/02_Lesson/04_Lesson04_Answer/Scripts/TriggerNotifier.cs
+++ Assets/UniRxHandsOn/Projects/02_Lesson/04_Lesson04_Answer/Scripts/TriggerNotifier.cs
@@ -2,24 +2,28 @@
 using UniRx;
 using UnityEngine;
 
-namespace UniRxHandsOn.Lesson_04_Answer {
-	public class TriggerNotifier : MonoBehaviour {
-		private Subject<Unit> m_OnEnter = new Subject<Unit> ();
-		public IObservable<Unit> OnEnter { get { return m_OnEnter; } }
+namespace UniRxHandsOn.Lesson_04_Answer
+{
+    public class TriggerNotifier : MonoBehaviour
+    {
+        private Subject<Unit> m_OnEnter = new Subject<Unit>();
+        public IObservable<Unit> OnEnter { get { return m_OnEnter; } }
 
-		private Subject<Unit> m_OnExit = new Subject<Unit> ();
-		public IObservable<Unit> OnExit { get { return m_OnExit; } }
+        private Subject<Unit> m_OnExit = new Subject<Unit>();
+        public IObservable<Unit> OnExit { get { return m_OnExit; } }
 
-		private void OnTriggerEnter (Collider other) {
-			if (other.tag != "Player") return;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag != "Player") return;
 
-			m_OnEnter.OnNext (Unit.Default);
-		}
+            m_OnEnter.OnNext(Unit.Default);
+        }
 
-		private void OnTriggerExit (Collider other) {
-			if (other.tag != "Player") return;
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag != "Player") return;
 
-			m_OnExit.OnNext (Unit.Default);
-		}
-	}
+            m_OnExit.OnNext(Unit.Default);
+        }
+    }
 }
